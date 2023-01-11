ec57f606f83e92afcc964edcf73213b85c193b3b
Gabe Daly <gabeedaly@gmail.com>
2023-01-11 16:19:14 -0500

    Updated from Github

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const exp = require('constants');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,9 @@ const app = express();
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
